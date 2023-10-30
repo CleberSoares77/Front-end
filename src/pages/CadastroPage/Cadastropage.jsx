9cf948685e87bb96f72ec4915236ee349af10fda
Cleber <cjss1@aluno.ifnmg.edu.br>
2023-10-30 00:48:34 -0300

    .

---
+++ src/pages/CadastroPage/Cadastropage.jsx
@@ -0,0 +1,34 @@
+import {
+    Container,
+    Box,
+    Center,
+    Heading,
+    Input,
+    Button,
+    Wrap
+} from "@chakra-ui/react"
+
+export function CadastroPage() {
+    return (
+        <Container
+            maxW='2xl'
+            centerContent
+            height='100vh'
+        >
+            <Center h='100vh'>
+                <Box padding='4' color='black' maxW='xl'>
+                    <Wrap spacing={4}>
+                        <Heading>
+                            Cleber
+                        </Heading>
+                        <Input
+                            placeholder="Select Date and Time"
+                            size="md"
+                            type="datetime-local"
+                        />
+                    </Wrap>
+                </Box>
+            </Center>
+        </Container>
+    )
+}
