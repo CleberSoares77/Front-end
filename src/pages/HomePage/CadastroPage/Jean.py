f095b362c6eeaaf8c04060841cf85d8dd0695c5f
Cleber <cjss1@aluno.ifnmg.edu.br>
2023-11-12 21:42:42 -0300

    ,

--- src/pages/HomePage/CadastroPage/Jean.py
+++ src/pages/HomePage/CadastroPage/Jean.py
@@ -1,9 +1,33 @@
 import flet as ft
-
-def main(pg: ft.Page):
-    pg.title = "Olá, Usuário!"
-    pg.vertical_alignment = ft.MainAxisAlignment.CENTER
-    btn = ft.ElevatedButton("Clique aqui, zé!")
-    pg.add(btn)
-
-ft.app(target=main, view=ft.WEB_BROWSER)
+def validade_requided_text_field(e):
+    if e.control.value=="" or len(e.control.value) > 3:
+        e.control.error_text = "Preenchimento obrigatório!"
+        e.control.updte()
+def create_from(pg):
+    vetor= [
+        ft.Radio(value="F", label="Feminino"),
+        ft.Radio(value="M", label="Masculino")
+    ]
+      
+    sex = ft.RadioGroup(
+        content=ft.Row(vetor)
+    )
+    return ft.SafeArea(
+        ft.Column(
+            scroll=ft.ScrollMode.AUTO,
+            controls=[
+                ft.TextField(
+                    label="Nome",
+                    keyboard_type=ft.KeyboardType.NAME,
+                    on_blur=validade_requided_text_field,
+                ),
+                sex
+            ]
+        ),
+        expand=True,
+    )
+def main(page: ft.Page):
+    page.title="Hello Flet!"
+    page.add(create_from(main))
+if __name__== "__main__":
+        ft.app(target=main, view=ft.WEB_BROWSER, port=8000)
